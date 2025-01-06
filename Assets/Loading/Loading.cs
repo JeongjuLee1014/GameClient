06bda2333e021f1bb85e52c867f1520039634302
jeongju <ryan20115@naver.com>
2025-01-06 09:23:35 +0900

    250106 Loading

--- Assets/Loading/Loading.cs
+++ Assets/Loading/Loading.cs
@@ -7,18 +7,18 @@ public class Loading : MonoBehaviour
     void Start()
     {
         // loginValues.json의 isLogined 값 읽어오기
-        const string filePath = "./Assets/login/loginValues.json";
+        const string filePath = "./Assets/Login/loginValues.json";
         string loginValuesString = File.ReadAllText(filePath);
         LoginValues loginValuesJson = JsonUtility.FromJson<LoginValues>(loginValuesString);
 
         // 로그인한 적이 있으면 Home 씬으로 이동, 그렇지 않으면 Login 씬으로 이동
         if (loginValuesJson.isLogined)
         {
-            SceneManager.LoadScene("Home");
+            //SceneManager.LoadScene("Home");
         }
         else
         {
-            SceneManager.LoadScene("Login");
+            //SceneManager.LoadScene("Login");
         }
     }
 }
